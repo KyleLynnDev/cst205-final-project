ec186dc5ce1d6b63457edec6368bc070ae055e7e
JTzeng1 <jtzeng@csumb.edu>
2025-12-08 17:28:16 -0800

    work done

--- app.py
+++ app.py
@@ -1,15 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from PIL import Image
+import os
+from datetime import datetime
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route('/wavefunctioncollapse')
 def wavefunctioncollapsepage():
     return render_template('WFC.html')
+
+@app.route('/pixelArt', methods=['GET'])
+def pixelArt_page():
+    return render_template("pixelArt.html")
+
+@app.route('/pixelArt', methods=['POST'])
+def pixelArt_image():
+    file = request.files['image']
+    pixel_size = int(request.form['pixel_size'])
+
+
+
+    img = Image.open(file)
+    width, height = img.size
+    pixelated = Image.new("RGB", (width, height))
+
+    for y in range(0, height, pixel_size):
+        for x in range(0, width, pixel_size):
+            r, g, b = img.getpixel((x, y))
+
+            for yy in range(y, min(y + pixel_size, height)):
+                for xx in range(x, min(x + pixel_size, width)):
+                    pixelated.putpixel((xx, yy), (r, g, b))
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    output_filename = f"pixelated_{timestamp}.png"
+    output_path = os.path.join(GALLERY_FOLDER, output_filename)
+    pixelated.save(output_path)
+
+  
+    return render_template("pixelArt.html", output_image=f"/{GALLERY_FOLDER}/{output_filename}")
+
+
+GALLERY_FOLDER = "static/gallery"
+os.makedirs(GALLERY_FOLDER, exist_ok=True)
+
+@app.route('/gallery')
+def gallery():
+    images = os.listdir(GALLERY_FOLDER)
+    images = [f"/{GALLERY_FOLDER}/{img}" for img in images]
+    return render_template("gallery.html", images=images)
 
 
 if __name__ == '__main__':
